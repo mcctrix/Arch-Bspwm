05a75d976e91ec4caf30433f8cacee45701f0939
Chandrashekhar Kachawa <42296139+mcctrix@users.noreply.github.com>
2020-06-30 18:59:01 +0530

    Create wallpaper_automation.sh

---
+++ .config/bspwm/wallpaper_automation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Channel_42
+#This script selects and applies a random wallaper from the ~/Pictures/Wallpapers Directory
+#Or it searches for a user-defined file
+
+DIR="/home/ctrix/Wallpaper/"   #Wallpaper Directory
+
+case "$1" in                    #check if user has inputed request
+    "")                         #if no imput, randowm wallpaper
+        cd "$DIR"               #change to wpp dir
+        WALLS=( $(ls) )         #create array with all wallpapers
+        RANDOM=$$$(date +%s)    #generate random number
+        selectedwall=${WALLS[$RANDOM % ${#WALLS[@]} ]}  #select a random wallpaper
+        final="${DIR}${selectedwall}"   #join strings for final command
+        ;; 
+    "-s")                       #show all Wallpapers user ui
+        selectedwall=$(ls  /home/ctrix/Wallpaper | rofi -dmenu -lines 3 -eh 2 -width 20 -theme /home/ctrix/.materia.rasi
+        final=("${DIR}${selectedwall}")
+        ;;
+    *)                          #if input, user wallpaper
+        if test -f "$DIR""$1"; then #check files existance
+            final="$DIR""$1"
+        else
+            echo "File not found"   #on error
+        fi
+esac
+
+feh --bg-fill "$final" &>/dev/null    #set wallpaper; supress errors
